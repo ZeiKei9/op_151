fa44425ce3bf62bcafac10c5ea212a1611762dc6
ZeiKei9 <artem.kovalv@internet.ru>
2024-03-21 14:23:10 +0300

    input, output / matrix, matrices

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -28,41 +28,8 @@ void inputMatrix(matrix *m);
 
 void outputMatrix(matrix m);
 
-void outputMatrixSum(matrix m, bool showSum);
-
 void inputMatrices(matrix *ms, int nMatrices);
 
 void outputMatrices(matrix *ms, int nMatrices);
-
-void swapRows(matrix m, int i1, int i2);
-
-void swapColumns(matrix m, int j1, int j2);
-
-int getSum(int *a, int n);
-int getSumCol(int **values, int nRows, int n);
-
-void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int*, int));
-
-void selectionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int*, int));
-
-bool isSquareMatrix(matrix *m);
-
-bool areTwoMatricesEqual(matrix *m1, matrix *m2);
-
-bool isEMatrix(matrix *m);
-
-bool isSymmetricMatrix(matrix *m);
-
-void transposeSquareMatrix(matrix *m);
-
-void transposeMatrix(matrix *m);
-
-position getMinValuePos(matrix m);
-
-position getMaxValuePos(matrix m);
-
-matrix createMatrixFromArray(const int *a, size_t nRows, size_t nCols);
-
-matrix *createArrayOfMatrixFromArray(const int *values,size_t nMatrices, size_t nRows, size_t nCols);
 
 #endif //LAB_MATRIX_H
